463859906165d2ecdb56886220269b739d25fde8
Fran <franciscopere987@gmail.com>
2022-08-27 19:54:09 -0300

    Added a testing test and babel config

---
+++ index.js
@@ -0,0 +1,1 @@
+//Archivo main del proyecto node
--- test/sample.test.js
+++ test/sample.test.js
@@ -1,3 +1,10 @@
+import {sum} from '../src/sampleCode'
+
+
 test('Ok', () => {
       expect(true)   
+})
+
+test('sum ok', () => {
+      expect(sum(1, 2)).toBe(3);
 })
